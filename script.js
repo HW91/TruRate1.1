20bb7fba8eca58c11c4b1569eb0a659c5b6f0126
HW91 <63923099+HW91@users.noreply.github.com>
2022-05-19 23:27:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
             // Load 1
 
-            fuction calculateDays(){
+            function calculateDays(){
             var d1= document.getElementById("d1").value;
             var d2= document.getElementById("d2").value;
             const dateOne = new Date(d1);
             const dateTwo = new Date(d2);
             const time = Math.abs(dateTwo - dateOne);
             const days = Math.ceil(time / (1000 * 60 * 60 24));
-            documnent.getElementById("output").innerHTML=days;
+            document.getElementById("output").innerHTML=days;
             }
 
             $('#ft-trim-1').on('input', function () {
